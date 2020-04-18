881b5718168066f9f3082c00590e44f8da70f2ce
Christian Ezeani <christian.ezeani@gmail.com>
2020-04-18 09:11:43 +0100

    Major Updates

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+try {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+  $dotenv->load();
+} catch (Exception $e) {
+  echo "{$e->getMessage()}\n";
+}
 
 require_once __DIR__.'/includes/OAuthTestCase.php';
